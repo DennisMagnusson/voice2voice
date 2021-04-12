e08a3154cd0137f0873c9a0da315bff76f44b85f
Dennis Magnusson <denmag@kth.se>
2021-04-12 12:55:17 +0200

    Make gan

--- train.py
+++ train.py
@@ -71,7 +71,7 @@ class MelGenerator(nn.Module):
 
     
 
-  def forward(self, x, hidden=None):
+  def forward(self, x):
     out = F.relu(self.encdropout0(self.enclinear0(x)))
     out = F.relu(self.encdropout1(self.enclinear1(out)))
 
@@ -101,41 +101,44 @@ class MelGenerator(nn.Module):
 
     return out, 0
 
-class AttentionThingy(nn.Module):
-  def __init__(self, in_size, hidden_size, out_size):
-    super(AttentionThingy, self).__init__()
+class Discriminator(nn.Module):
+  def __init__(self, seq_len, n_mels=80):
+    super(Discriminator, self).__init__()
+    # 80 x 256 x 1
+    self.conv0 = Conv2DThingy(1, 64, 5, nn.LeakyReLU(0.2), stride=2)
+    # 40 x 128 x 64
+    self.conv1 = Conv2DThingy(64, 64, 5, nn.LeakyReLU(0.2), stride=2)
+    # 20 x 64 x 128
+    self.conv2 = Conv2DThingy(64, 128, 5, nn.LeakyReLU(0.2), stride=2)
+    # 10 x 32 x 256
+    self.conv3 = Conv2DThingy(128, 256, 5, nn.LeakyReLU(0.2), stride=2)
+    # 5 x 16 x 256
+    self.conv4 = Conv2DThingy(256, 1, (seq_len//16, 5), None, stride=2)
 
-    self.dropout0 = nn.Dropout(0.3)
-    self.dropout1 = nn.Dropout(0.3)
-    self.prenet0 = nn.Linear(in_size, in_size)
-    self.prenet1 = nn.Linear(in_size, hidden_size)
+    self.linear = nn.Linear(5*16*256, 1, bias=False)
 
-    self.attnrnn = nn.GRU(hidden_size, hidden_size, 1, batch_first=True)
-
-    self.decrnn0 = nn.GRU(hidden_size, hidden_size, 1, batch_first=True)
-    self.decrnn1 = nn.GRU(hidden_size, out_size, 1, batch_first=True)
-
-    #self.pad = nn.ConstantPad1d(((kernel_size-1)//2, kernel_size//2), 0)
-    #self.conv = nn.Conv2d(in_size, out_size, kernel_size, padding=kernel_size//2)
-
-  def forward(self, x, h0=None, h1=None, h2=None):
-    out = F.relu(self.dropout0(self.prenet0(x)))
-    out = F.relu(self.dropout1(self.prenet1(out)))
-
-    out, h0 = self.attnrnn(out, hx=h0)
-    out0, h1 = self.decrnn0(out, hx=h1)
-    out, h2 = self.decrnn1(out0 + out, hx=h2)
-
-    return out, h0, h1, h2
-
+  def forward(self, x):
+    out = x.unsqueeze(1)
+    out = self.conv0(out)
+    out = self.conv1(out)
+    out = self.conv2(out)
+    out = self.conv3(out)
+    #out = self.conv4(out)
+    out = out.flatten(start_dim=1)
+    out = self.linear(out)
+    return torch.sigmoid(out)
 
 class Conv2DThingy(nn.Module):
-  def __init__(self, in_size, out_size, kernel_size, activation):
+  def __init__(self, in_size, out_size, kernel_size, activation, stride=1):
     super(Conv2DThingy, self).__init__()
     self.bn = nn.BatchNorm2d(out_size)
     self.activation = activation
-    #self.pad = nn.ConstantPad1d(((kernel_size-1)//2, kernel_size//2), 0)
-    self.conv = nn.Conv2d(in_size, out_size, kernel_size, padding=kernel_size//2)
+    if type(kernel_size) == tuple:
+      padding = 0
+    else:
+      padding = kernel_size//2
+
+    self.conv = nn.Conv2d(in_size, out_size, kernel_size, padding=padding, stride=stride)
 
   def forward(self, x):
     #out = self.pad(x)
@@ -144,8 +147,6 @@ class Conv2DThingy(nn.Module):
     if self.activation:
       return self.activation(out)
     return out
-
-
 
 class ConvThingy(nn.Module):
   def __init__(self, in_size, out_size, kernel_size, activation):
@@ -175,49 +176,6 @@ class Highway(nn.Module):
 
     return x0 * x1 + x*(1.0 - x1)
 
-class CBHG(nn.Module):
-  def __init__(self, size, k):
-    super(CBHG, self).__init__()
-
-    convies = []
-    for i in range(1, k+1):
-      convies.append(ConvThingy(size, size, i, nn.ReLU()))
-    
-    self.convies = nn.ModuleList(convies)
-
-    pool_size = 2
-    self.poolpad = nn.ConstantPad1d(((pool_size-1)//2, pool_size//2), 0)
-    self.pool = nn.MaxPool1d(pool_size, stride=1)
-
-    self.proj1 = ConvThingy(k*size, size, 3, nn.ReLU())
-    self.proj2 = ConvThingy(size, size, 3, None)
-
-    highways = []
-    for i in range(4):
-      highways.append(Highway(size))
-    self.highways = nn.ModuleList(highways)
-    self.gru = nn.GRU(size, size, 1, dropout=0.1, batch_first=True, bidirectional=True)
-    
-  def forward(self, x):
-    out = x.transpose(1, 2)
-    outs = []
-    for conv in self.convies:
-      outs.append(conv(out))
-
-    out = torch.cat(outs, dim=1)
-    out = self.pool(self.poolpad(out))
-    out = self.proj1(out)
-    out = self.proj2(out)
-    
-    out = out.transpose(1, 2)
-
-    out += x
-    for h in self.highways:
-      out = h(out)
-
-    out, _ = self.gru(out)
-    return out
-
 def generate(model, phoneme_classifier, vocoder, name='1it', device='cpu', k=1):
   files = [a for a in ['LJ042-0033.wav', 'LJ008-0195.wav', 'LJ050-0174.wav']]
   model.eval()
@@ -228,7 +186,6 @@ def generate(model, phoneme_classifier, vocoder, name='1it', device='cpu', k=1):
     pass
 
   for filename in files:
-    #mel = get_mel('./speech_data/LJSpeech-1.1/wavs/'+filename, config_filename='./UniversalVocoding/config2.json')
     wav, sr = get_wav('./speech_data/LJSpeech-1.1/wavs/'+filename)
     mel = wav_to_mel(wav, sr).T
     mel_vocoder = vocoder(torch.Tensor([wav]))[0].T
@@ -237,13 +194,10 @@ def generate(model, phoneme_classifier, vocoder, name='1it', device='cpu', k=1):
     with torch.no_grad():
       phones, _ = phoneme_classifier(torch.Tensor([mel]).to(device))
       phones = phones.to(device)
-      #phones = F.softmax(phones/0.7, dim=1)
       #inpu = torch.cat((phones.transpose(1, 2), torch.Tensor(f0), torch.Tensor(prob)), 1)
       inpu = torch.cat((phones.transpose(1, 2), f0.unsqueeze(0).unsqueeze(2)), 2)
       mel2, _ = model(inpu)
-      #print(mel2.shape)
       
-      #for ex in [1.0, 1.25, 1.5, 1.75, 2.0]:
       for ex in [1.0]:
         mel3 = torch.pow(mel2, ex)
 
@@ -311,53 +265,106 @@ def main(device='cpu', batch_size=32):
     phoneme_classifier = PhonemeClassifier().to(device)
     phoneme_classifier.load_state_dict(torch.load('./phoneme_classifier/models/phoneme-classifier-final64-state_dict-30ep.pth'))
 
-    #phoneme_classifier = torch.load('./phoneme_classifier/models/phoneme-classifier--final2-model-50ep.pth')['model']
-    phoneme_classifier.eval()
-
-    vocoder = get_vocoder()#torch.hub.load('descriptinc/melgan-neurips', 'load_melgan')
-
-    model = MelGenerator().to(device)
-    wandb.watch(model)
-    #model.load_state_dict(torch.load('./models/mel-generator-state_dict-500it.pth'))
-    lr = wandb.config.lr = 4e-4
-
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    #optimizer = things['optimizer']
-    #sched = things['scheduler']
-
-    sched = ReduceLROnPlateau(optimizer, factor=0.8, patience=300, threshold=0.000000001, verbose=True)
-    #criterion = nn.MSELoss()
-    criterion = nn.L1Loss()
-    torch.autograd.set_detect_anomaly(True)
-
     smooth_loss = 0.15
     batch_size = wandb.config.batch_size = batch_size
     seq_len = wandb.config.seq_len = 256
     fmax = wandb.fmax = 500
     k = wandb.k = 1
     counter = 0
+
+
+    #phoneme_classifier = torch.load('./phoneme_classifier/models/phoneme-classifier--final2-model-50ep.pth')['model']
+    phoneme_classifier.eval()
+
+    vocoder = get_vocoder()#torch.hub.load('descriptinc/melgan-neurips', 'load_melgan')
+
+    model = MelGenerator().to(device)
+    disc = Discriminator(seq_len)
+    wandb.watch(model)
+    #model.load_state_dict(torch.load('./models/mel-generator-state_dict-500it.pth'))
+    #lr = wandb.config.lr = 4e-4
+    lr = wandb.config.lr = 0.001
+
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    d_optimizer = optim.Adam(disc.parameters(), lr=lr)
+    g_optimizer = optim.Adam(model.parameters(), lr=lr)#XXX Dont know if this can be same as first optimizer?
+
+    #optimizer = things['optimizer']
+    #sched = things['scheduler']
+
+    sched = ReduceLROnPlateau(optimizer, factor=0.8, patience=300, threshold=0.000000001, verbose=True)
+    #criterion = nn.MSELoss()
+    gen_criterion = nn.L1Loss()
+    criterion = nn.BCELoss()
+    #criterion = nn.MSELoss()
+    #torch.autograd.set_detect_anomaly(True)
+
     for ep in range(10000):
       for batch in get_batches(phoneme_classifier, vocoder, batch_size=batch_size, seq_len=seq_len, fmax=fmax, k=k, device=device):
         x, y = batch
-        #x = torch.Tensor(x).to(device)
-        #y = torch.Tensor(y).to(device)
         counter += 1
-        hx = None
         retain_graph = True
 
-        optimizer.zero_grad()
-        logits, _ = model.forward(x, hidden=hx)
-        loss = criterion(logits, y)
-        
-        #loss /= batch_size
-        smooth_loss = smooth_loss*0.95 + loss.item()*0.05
-        loss.backward(retain_graph=retain_graph)
-        optimizer.step()
+        if counter < 50:# Pretrain generator
+          model.zero_grad()
+          fake_mel, _ = model.forward(x)
+          loss = gen_criterion(fake_mel, y)
 
-        wandb.log({'loss': loss.item()**2})
+          #loss /= batch_size
+          smooth_loss = smooth_loss*0.95 + loss.item()*0.05
+          loss.backward()
+          optimizer.step()
+          
+          if counter > 10:
+            disc.zero_grad()
+            #d_optimizer.zero_grad()
+            outputs = disc.forward(y)
+            d_loss_real = criterion(outputs, torch.ones_like(outputs))
+            d_loss_real.backward()
+            # Train on fakes
+            fake_mel, _ = model.forward(x)
+            outputs = disc.forward(fake_mel.detach()).view(-1)
+            d_loss_fake = criterion(outputs, torch.zeros_like(outputs))
+            d_loss_fake.backward()
+            d_optimizer.step()
+
+          wandb.log({'loss': loss.item()**2})
+        else:# Train everything
+          # Train discriminator
+          # Train on reals
+          disc.zero_grad()
+          outputs = disc.forward(y)
+          d_loss_real = criterion(outputs, torch.ones_like(batch_size))
+          d_loss_real.backward()
+          # Train on fakes
+          fake_mels, _ = model.forward(x)
+          outputs = disc.forward(fake_mels.detach())
+          d_loss_fake = criterion(outputs, torch.zeros_like(batch_size))
+          d_loss_fake.backward()
+          d_optimizer.step()
+          # Train generator
+          model.zero_grad()
+
+          outputs = disc.forward(fake_mels)
+          g_loss = criterion(outputs, torch.ones_like(outputs))
+          gen_loss = gen_criterion(fake_mels, y)
+          tot_loss = g_loss + gen_loss
+          tot_loss.backward()
+          g_optimizer.step()
+          print(outputs)
+
+          di = {'d_loss':(d_loss_real.mean().item() + d_loss_fake.mean().item())/2.0,
+                     'g_loss': g_loss.mean().item(),
+                     'loss': gen_loss.mean().item()**2}
+
+          wandb.log(di)
+          print(di)
+
+          pass
 
         
-        sched.step(loss.item())
+        #sched.step(loss.item())
 
         if counter % 25 == 0 or counter == 1:
           print('iter: {} loss: {}, smooth_loss: {}'.format(counter, loss.item(), smooth_loss))
@@ -366,7 +373,7 @@ def main(device='cpu', batch_size=32):
             saved_thing = {'model': model, 'optimizer': optimizer, 'scheduler': sched}
             torch.save(model.state_dict(), './models/mel-generator-state_dict-{}it.pth'.format(counter))
             torch.save(saved_thing, './models/mel-generator-model-{}it.pth'.format(counter))
-            generate(model, phoneme_classifier, vocoder, name='{}it'.format(counter), device=device, k=k)
+            #generate(model, phoneme_classifier, vocoder, name='{}it'.format(counter), device=device, k=k)
       print('batch {} done'.format(str(ep)))
   
 if __name__ == '__main__':
